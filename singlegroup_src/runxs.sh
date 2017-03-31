798f1554b8c9267e22a8dd016287fdc236027075
yingyiluo <yingyiluo2015@u.northwestern.edu>
2017-03-30 20:02:14 -0500

    changed running dir

--- singlegroup_src/runxs.sh
+++ singlegroup_src/runxs.sh
@@ -1,5 +1,5 @@
 
-cp /var/tmp/XSBench_aocx/base/CalculateXS.aocx .
+cp /opt/XSBench_aocx/base/CalculateXS.aocx .
 ./XSBench -t 15000000 | tee results/base.txt
 start_line=$(grep 'OCL_KERNEL_TS_SEC' results/base.txt)
 end_line=$(grep 'OCL_POST_TS_SEC' results/base.txt)
@@ -7,7 +7,7 @@ start_time=`echo "$start_line" | sed 's/^.*OCL_KERNEL_TS_SEC=//'`
 end_time=`echo "$end_line" | sed 's/^.*OCL_POST_TS_SEC=//'`
 ssh knight ~/gitwork/ocl-iabench/wt310script/wt310samples_sqlite.py $start_time $end_time ~/base.txt 
 
-cp /var/tmp/XSBench_aocx/cc2mgridpoint/CalculateXS.aocx .
+cp /opt/XSBench_aocx/cc2mgridpoint/CalculateXS.aocx .
 ./XSBench -t 15000000 | tee results/cc2mgridpoint.txt
 start_line=$(grep 'OCL_KERNEL_TS_SEC' results/cc2mgridpoint.txt)
 end_line=$(grep 'OCL_POST_TS_SEC' results/cc2mgridpoint.txt)
@@ -16,7 +16,7 @@ end_time=`echo "$end_line" | sed 's/^.*OCL_POST_TS_SEC=//'`
 ssh knight ~/gitwork/ocl-iabench/wt310script/wt310samples_sqlite.py $start_time $end_time ~/cc2mgridpoint.txt
 
 type=fma 
-cp /var/tmp/XSBench_aocx/$type/CalculateXS.aocx .
+cp /opt/XSBench_aocx/$type/CalculateXS.aocx .
 ./XSBench -t 15000000 | tee results/$type.txt
 start_line=$(grep 'OCL_KERNEL_TS_SEC' results/$type.txt)
 end_line=$(grep 'OCL_POST_TS_SEC' results/$type.txt)
@@ -25,7 +25,7 @@ end_time=`echo "$end_line" | sed 's/^.*OCL_POST_TS_SEC=//'`
 ssh knight ~/gitwork/ocl-iabench/wt310script/wt310samples_sqlite.py $start_time $end_time ~/$type.txt
 
 type=optbsearch
-cp /var/tmp/XSBench_aocx/$type/optbCalculateXS.aocx .
+cp /opt/XSBench_aocx/$type/optbCalculateXS.aocx .
 ./XSBench_optb -t 15000000 | tee results/$type.txt
 start_line=$(grep 'OCL_KERNEL_TS_SEC' results/$type.txt)
 end_line=$(grep 'OCL_POST_TS_SEC' results/$type.txt)
@@ -34,7 +34,7 @@ end_time=`echo "$end_line" | sed 's/^.*OCL_POST_TS_SEC=//'`
 ssh knight ~/gitwork/ocl-iabench/wt310script/wt310samples_sqlite.py $start_time $end_time ~/$type.txt
 
 type=vectornucs
-cp /var/tmp/XSBench_aocx/$type/optbCalculateXS.aocx .
+cp /opt/XSBench_aocx/$type/optbCalculateXS.aocx .
 ./XSBench_optb_vector -t 15000000 | tee results/$type.txt
 start_line=$(grep 'OCL_KERNEL_TS_SEC' results/$type.txt)
 end_line=$(grep 'OCL_POST_TS_SEC' results/$type.txt)
